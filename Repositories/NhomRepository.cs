5a6c9725be661d2d9b76c116736eee1d6d2d152a
puahuhu <nguyenletuanphuongzzz@gmail.com>
2024-03-29 17:27:50 +0700

    Phuong - commit - check InformationEditUtilities.cs

---
+++ Repositories/NhomRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositories.Entities;
+
+namespace Repositories
+{
+    public class NhomRepository
+    {
+        TasManaContext db = new TasManaContext();
+        public Nhom? Get(string groupID)
+        {
+            // Access DB
+            return db.Nhoms.FirstOrDefault(x => x.MaNhom.Equals(groupID));
+        }
+    }
+}
