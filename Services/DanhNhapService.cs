4afdc22fac6b7fe0f8643b739c432b078004a133
PhuocPhung <phuocphung5890@gmail.com>
2024-04-03 15:23:44 +0700

    luu lần nữa

---
+++ Services/DanhNhapService.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Entities;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class DanhNhapService
+    {
+        DangNhapRepository repository = new DangNhapRepository();
+        public string checkLogin(string username, string password)
+        {
+            return repository.check(username, password);
+        }
+        public bool savePassword(string username, string password)
+        {
+            return repository.save(username, password);
+        }
+    }
+}
---
+++ UIs/G_Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Services;
+
+namespace UIs
+{
+    public partial class G_Login : Form
+    {
+        public G_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Login_Click(object sender, EventArgs e)
+        {
+            DanhNhapService service = new DanhNhapService();
+            string userID = box_username.Text;
+            string password = box_password.Text;
+            string result = service.checkLogin(userID, password);
+            MessageBox.Show(result);
+        }
+    }
+}
