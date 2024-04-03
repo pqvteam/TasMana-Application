d84c862ae4d179c7b7d764f5d741cb994bdfcab6
PhuocPhung <phuocphung5890@gmail.com>
2024-04-03 19:09:01 +0700

    DangNhap xong

--- Repositories/DangNhapRepository.cs
+++ Repositories/DangNhapRepository.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Data.SqlClient;
 using Repositories.Entities;
 using Repositories.Utilities;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 
 namespace Repositories
@@ -14,6 +16,8 @@ namespace Repositories
     {
         public string check(string username, string password)
         {
+            try
+            {
                 DatabaseConnection.Instance.OpenConnection();
                 SqlConnection conn = DatabaseConnection.Instance.GetConnection();
                 string sqlCommand = $"DECLARE @response NVARCHAR(30);" +
@@ -23,23 +27,24 @@ namespace Repositories
 
                 if (reader.Read())
                 {
-                    string response = $"{reader["Response"]}";
-                    //
-                    return response;
+                    return $"{reader["Response"]}";
                 }
-            DatabaseConnection.Instance.CloseConnection();
-            return "Lỗi truy cập dữ liệu1";
-            
-
+                return "Lỗi kết nối!!";
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi kết nối!!";
+            }
+            finally { DatabaseConnection.Instance.CloseConnection(); }
         }
-        public bool save(string username, string password)
+        public bool savePassword(string username, string password)
         {
             try
             {
                 DatabaseConnection.Instance.OpenConnection();
                 SqlConnection conn = DatabaseConnection.Instance.GetConnection();
 
-                string sqlCommand = $"insert into LuuMatKhau(userID,matkhau) values('" + username + "','" + password + "')";
+                string sqlCommand = $"insert into LuuMatKhau(userID,matkhau) values('{username}','{password}')";
                 SqlCommand command = new SqlCommand(sqlCommand, conn);
                 int reader = command.ExecuteNonQuery();
                 if (reader > 0)
@@ -51,6 +56,115 @@ namespace Repositories
             catch (Exception ex)
             {
                 return false;
+            }
+            finally
+            {
+                DatabaseConnection.Instance.CloseConnection();
+            }
+        }
+
+        public string sendCode(string username)
+        {
+            try
+            {
+                DatabaseConnection.Instance.OpenConnection();
+                SqlConnection conn = DatabaseConnection.Instance.GetConnection();
+
+                string sqlCommand = $"select email from NhanSu where maThanhVien='{username}'";
+                SqlCommand command = new SqlCommand(sqlCommand, conn);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return $"{reader["email"]}";
+                }
+                return "Nhân viên này không tồn tại";
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi kết nối dữ liệu";
+            }
+            finally
+            {
+                DatabaseConnection.Instance.CloseConnection();
+            }
+        }
+        public bool saveCode(string username, string code)
+        {
+            try
+            {
+                DatabaseConnection.Instance.OpenConnection();
+                SqlConnection conn = DatabaseConnection.Instance.GetConnection();
+
+                string sqlCommand = $"update LuuMatKhau set maxacnhan='{code}' where userID='{username}'";
+                SqlCommand command = new SqlCommand(sqlCommand, conn);
+                int reader = command.ExecuteNonQuery();
+
+                if (reader > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                DatabaseConnection.Instance.CloseConnection();
+            }
+        }
+        public bool confirmCode(string username ,string code)
+        {
+            try
+            {
+                DatabaseConnection.Instance.OpenConnection();
+                SqlConnection conn = DatabaseConnection.Instance.GetConnection();
+
+                string sqlCommand = $"select maxacnhan from LuuMatKhau where userID='{username}'";
+                SqlCommand command = new SqlCommand(sqlCommand, conn);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    string maXacNhan = $"{reader["maXacNhan"]}";
+                    if(code.Equals(maXacNhan))
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                DatabaseConnection.Instance.CloseConnection();
+            }
+        }
+        public string getPassword(string username)
+        {
+            try
+            {
+                DatabaseConnection.Instance.OpenConnection();
+                SqlConnection conn = DatabaseConnection.Instance.GetConnection();
+
+                string sqlCommand = $"select matkhau from LuuMatKhau where userID='{username}'";
+                SqlCommand command = new SqlCommand(sqlCommand, conn);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    return $"{reader["matkhau"]}";  
+                }
+                return "Lỗi kết nối dữ liệu!";
+            }
+            catch (Exception ex)
+            {
+                return "Lỗi kết nối dữ liệu!!";
             }
             finally
             {
--- Services/DanhNhapService.cs
+++ Services/DanhNhapService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Services;
 
 namespace Services
 {
@@ -17,7 +18,35 @@ namespace Services
         }
         public bool savePassword(string username, string password)
         {
-            return repository.save(username, password);
+            return repository.savePassword(username, password);
+        }
+        public string sendConfirmCode(string username)
+        {
+            MailService mailService = new MailService();
+            Random random = new Random();
+
+            // Tạo số ngẫu nhiên có 6 chữ số
+            int randomNumber = random.Next(100000, 999999);
+            string mailAddress = repository.sendCode(username);
+            repository.saveCode(username, randomNumber.ToString());
+            string content = "Mã xác nhận tài khoản đăng nhập của bạn là: " + randomNumber.ToString();
+            if (mailAddress != "Lỗi kết nối dữ liệu" && mailAddress != "Nhân viên này không tồn tại")
+            {
+                mailService.sendMail("Gửi mã xã nhận khôi phục mật khẩu", content, mailAddress);
+                return "Mã xác nhận đã được gửi đến email của bạn";
+            }
+            else
+            {
+                return "Không tìm thấy mail của nhân viên này! Kiểm tra lại mã đăng nhập!";
+            }
+        }
+        public string confirmCode(string username, string code) 
+        { 
+            if(repository.confirmCode(username, code))
+            {
+                return repository.getPassword(username);
+            }
+            return "Mã xác nhận sai!";
         }
     }
 }
