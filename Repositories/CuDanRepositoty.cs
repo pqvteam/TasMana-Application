0651d5735a7703ba1a346ef5a27d5e0c57a79827
dukelw <thevi16102004@gmail.com>
2024-05-01 01:42:05 +0700

    complete statistic

---
+++ Repositories/CuDanRepositoty.cs
@@ -0,0 +1,23 @@
+﻿using Repositories.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class CuDanRepositoty
+    {
+        TasManaContext db = new TasManaContext();
+
+        public CuDan? Get(string ID)
+        {
+            return db.CuDans.FirstOrDefault(x => x.MaCuDan == ID);
+        }
+        public List<CuDan> GetAll()
+        {
+            return db.CuDans.ToList();
+        }
+    }
+}
--- UIs/Program.cs
+++ UIs/Program.cs
@@ -14,8 +14,9 @@ static class Program
         // Application.Run(new M_AssignTask());
         // Application.Run(new A_ShowMember());
         // Application.Run(new A_ShowVenue());
-        Application.Run(new G_Login());
+        // Application.Run(new G_Login());
         // Application.Run(new A_EditTask());
         // Application.Run(new A_ShowDepartment());
+        Application.Run(new A_Statistic());
     }
 }
